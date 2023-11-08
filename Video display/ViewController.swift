8d7fe14c642cb97fd8780f51e0ede1065ad0e036
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-11-08 19:54:15 +0100

    Setup video view

--- Video display/ViewController.swift
+++ Video display/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let videoView: UIView = {
+        let view = UIView()
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setVideoView()
     }
-
-
+    
+    private func setVideoView() {
+        view.addSubview(videoView)
+        videoView.frame = CGRect(x: 5,
+                                 y: 100,
+                                 width: view.frame.size.width - 10,
+                                 height: view.frame.size.width - 10)
+        videoView.backgroundColor = .blue
+    }
 }
 
