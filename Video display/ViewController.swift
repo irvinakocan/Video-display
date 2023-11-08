2abf9ba5e8b3c9dc8f20ac8efe29bb7c42f6a7b2
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2023-11-08 19:31:23 +0100

    Initial Commit

---
+++ Video display/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Video display
+//
+//  Created by Macbook Air 2017 on 8. 11. 2023..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
